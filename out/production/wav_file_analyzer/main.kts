50057edbd1a01c0e36195ca57fc64f7684dc54fd
Fumiya Kume <fumiya.kume@hotmail.com>
2019-04-08 18:02:49 +0900

    基本的な情報を取得できるようにした

---
+++ out/production/wav_file_analyzer/main.kts
@@ -0,0 +1,46 @@
+import java.io.File
+import kotlin.system.exitProcess
+
+val file = File("cat1.wav")
+val fileExits = file.exists()
+
+if(!fileExits){
+    exitProcess(-1)
+}
+
+// ファイルを読み込むことができるか
+
+val fileStream = file.readBytes()
+
+if(fileStream.isEmpty()){
+    exitProcess(-2)
+}
+
+val chunkID = fileStream.copyOfRange(0,4)
+val chunkIDString = chunkID.map { it.toChar() }.fold(""){ r, i -> r + i }
+println("chunkIDString: ${chunkIDString}")
+
+val chunkSizeBytes = fileStream.copyOfRange(4,7)
+
+val format = fileStream.copyOfRange(8,11).map { it.toChar() }.fold("",{r,i -> r + i})
+println("format: ${format}")
+
+val subChunkId = fileStream.copyOfRange(12,15).map { it.toChar() }.fold("",{r,i -> r + i})
+println("subChunkID: ${subChunkId}")
+
+val audioFormat = fileStream.copyOfRange(20,22).elementAt(0)
+val audioFormatString = when(audioFormat){
+    1.toByte() -> "PCM"
+    else -> "No infomation for audio format"
+}
+
+println("Audio format: ${audioFormatString}")
+
+val audioNumber = fileStream.copyOfRange(22,24).elementAt(0)
+val audioNumberString = when(audioNumber){
+    1.toByte() -> "Mono"
+    2.toByte() -> "Steleo"
+    else -> "No audio Number Infomation"
+}
+
+println(audioNumberString)
